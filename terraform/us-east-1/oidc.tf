97858c37090d1f905c639d8c822465cb41c80a45
Steven Lam <34800247+StvnLm@users.noreply.github.com>
2026-06-05 15:45:16 -0400

    Add github workflow for CI (#4)
    * Add github workflow for CI
    
    ---------
    
    Co-authored-by: Stevlam <stevlam@cisco.com>

--- terraform/us-east-1/oidc.tf
+++ terraform/us-east-1/oidc.tf
@@ -1,15 +1,15 @@
 locals {
-  github_owner = "stvnlm"
-  github_repo  = "terraform-daycarewatch"
+  github_owner  = "StvnLm"
+  github_repo   = "terraform-daycarewatch"
   github_branch = "main"
 
-  role_name  = "github-actions-${local.github_repo}"
+  role_name = "github-actions-${local.github_repo}"
 
   github_subject = "repo:${local.github_owner}/${local.github_repo}:ref:refs/heads/${local.github_branch}"
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
+  url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
 }
 
@@ -29,11 +29,59 @@ data "aws_iam_policy_document" "github_actions_trust" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = [local.github_subject]
+      values = [
+        "repo:${local.github_owner}/${local.github_repo}:ref:refs/heads/main",
+        "repo:${local.github_owner}/${local.github_repo}:ref:refs/heads/feature/*",
+        "repo:${local.github_owner}/${local.github_repo}:pull_request",
+        "repo:${local.github_owner}/${local.github_repo}:environment:env-var",
+      ]
     }
   }
+}
+
+resource "aws_iam_role_policy" "github_actions_backend_access" {
+  name = "terraform-backend-access"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::terraform-daycarewatch"
+        Condition = {
+          StringLike = {
+            "s3:prefix" = [
+              "us-east-1/terraform.tfstate",
+              "us-east-1/terraform.tfstate.tflock"
+            ]
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "arn:aws:s3:::terraform-daycarewatch/us-east-1/terraform.tfstate"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::terraform-daycarewatch/us-east-1/terraform.tfstate.tflock"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "github_actions" {
